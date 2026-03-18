82ba35cac34a79862f6b226e3a126bdbb50e01e0
Emma-267 <ek3833@srmist.edu.in>
2026-03-18 09:36:42 +0530

    UC3: Track Unique Bogie IDs (Set – HashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,25 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-//Allow dynamic insertion and removal of passenger bogies using ArrayList.
-//@version 2.0
+//Ensure no duplicate bogie IDs are added to the train.
+//@version 3.0
 public class TrainConsistManagementApp{
     public static void main(String[] args){
-        System.out.println("=========================================");
-        System.out.println("== UC2 - Add Passenger Bogies to Train ==");
-        System.out.println("=========================================\n");
-        List<String> passengerBogies=new ArrayList<>();
-        System.out.println("After Adding Bogies:");
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-        System.out.println("Passenger Bogies: "+passengerBogies);
-        System.out.println("\nAfter Removing 'AC Chair:");
-        passengerBogies.remove("AC Chair");
-        System.out.println("Passenger Bogies: "+passengerBogies);
-        System.out.println("\nChecking if 'Sleeper exists:");
-        System.out.println("Contains Sleeper?: "+passengerBogies.contains("Sleeper"));
-        System.out.println("\nFinal Train Passenger Consist:\n"+passengerBogies);
-        System.out.println("\nUC2 operations completed successfully...");
+        System.out.println("==================================");
+        System.out.println("== UC3 - Track Unique Bogie IDs ==");
+        System.out.println("==================================\n");
+        Set<String> bogies=new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+        System.out.println("Bogies IDs After Insertion:\n"+bogies);
+        System.out.println("\nNote:\nDuplicates are automatically ignored by HashSet.");
+        System.out.println("\nUC2 uniqueness validation completed...");
     }
 }
